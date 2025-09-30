c98b951cdadc7fa9cd857d9eb4a10bc47d3ef35b
seongwonjung <jsjsw0918@naver.com>
2025-09-30 21:31:24 +0900

    Feat: vm_stack_growth함수 addr 내림

--- pintos/vm/vm.c
+++ pintos/vm/vm.c
@@ -220,7 +220,8 @@ static bool vm_do_claim_page(struct page *page)
     page->frame = frame;
 
     /* TODO: Insert page table entry to map page's VA to frame's PA. */
-    pml4_set_page(thread_current()->pml4, page->va, frame->kva, page->writable);
+    if (!pml4_set_page(thread_current()->pml4, page->va, frame->kva, page->writable))
+        return false;
 
     return swap_in(page, frame->kva);
 }
