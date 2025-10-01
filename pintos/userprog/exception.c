8655ec6a3baa83bc6a557b224f9ae29ce381ca91
crucial-sub <jssub940@gmail.com>
2025-10-01 21:06:56 +0900

    feat: 커널의 유저 스택 접근 폴트 처리 기능 구현 #46

--- pintos/userprog/exception.c
+++ pintos/userprog/exception.c
@@ -146,6 +146,14 @@ static void page_fault(struct intr_frame *f)
         return;
 #endif
 
+    if (is_user_vaddr(fault_addr))
+    {
+        struct thread *cur = thread_current();
+        cur->exit_status = -1;
+        printf("%s: exit(%d)\n", cur->name, cur->exit_status);
+        thread_exit();
+    }
+
     /* Count page faults. */
     page_fault_cnt++;
     // 커널영역에서 실패 했을 경우 thread_exit()
